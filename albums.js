5e5e1e829b9a5cdb7415215062603b80a5d26ec9
adrien <adrien@arch-virtualbox>
2017-02-04 18:03:07 -0500

    adding skeletton

---
+++ albums.js
@@ -0,0 +1,7 @@
+var albums = [
+  '6AvkC0h2TUdX6fpAKtXqYR',     // Castlevanie Symphone of the Night
+  '70kaIuCoHOZ11adVwGQroA',     // Sunset Overdrive
+  '5LSmyO0hA8dsgk4g2KS2sX',     // Sonic Generations Vol. 1
+  '1AL4h5rN3vis3unZLvKVjm',     // Mass Effect 2
+  '2r4a3PREYIRF2QdbcPnrEO'      // Bastion
+];
