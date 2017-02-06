d885f842c305cc64dfd0517ba033ae889212df1b
Adrien Brochard <adrien.brochard@c9tec.com>
2017-02-06 10:38:24 -0500

    adding more albums and breaking into multiple batch API calls

--- albums.js
+++ albums.js
@@ -1,7 +1,27 @@
 var albums = [
-  '6AvkC0h2TUdX6fpAKtXqYR',     // Castlevanie Symphone of the Night
-  '70kaIuCoHOZ11adVwGQroA',     // Sunset Overdrive
-  '5LSmyO0hA8dsgk4g2KS2sX',     // Sonic Generations Vol. 1
-  '1AL4h5rN3vis3unZLvKVjm',     // Mass Effect 2
-  '2r4a3PREYIRF2QdbcPnrEO'      // Bastion
+  'https://open.spotify.com/album/6AvkC0h2TUdX6fpAKtXqYR', // Castlevanie Symphone of the Night
+  'https://open.spotify.com/album/70kaIuCoHOZ11adVwGQroA', // Sunset Overdrive
+  'https://open.spotify.com/album/5LSmyO0hA8dsgk4g2KS2sX', // Sonic Generations Vol. 1
+  'https://open.spotify.com/album/7d6hwzyHCXIE3n3y0e3r8m', // Sonic Generations Vol. 2
+  'https://open.spotify.com/album/2CaTyv2PRp5uG5SHIb2tK5', // Mass Effect
+  'https://open.spotify.com/album/1AL4h5rN3vis3unZLvKVjm', // Mass Effect 2
+  'https://open.spotify.com/album/7fvGyFc1ike0zd2pE7q9lr', // Mass Effect 3
+  'https://open.spotify.com/album/2r4a3PREYIRF2QdbcPnrEO', // Bastion
+  'https://open.spotify.com/album/3oVKqk9TahxvHpFIUlAEuJ', // Jet Set Radio
+  'https://open.spotify.com/album/3YXJBeqOWrKCi3DIDOgJw0', // Jet Set Radio Future
+  'https://open.spotify.com/album/0knSmwGyDZvvzg37rhzIpy', // Alex Kidd Vol. 1
+  'https://open.spotify.com/album/38PUHL4Ab0rky3W6Lnmp4r', // Out Run Vol. 1
+  'https://open.spotify.com/album/0ndOKj9ShVUgDc2UiR8b2M', // Journey
+  'https://open.spotify.com/album/63CH3LUNguGmuTG4jKpXAC', // Deus Ex: Human Revolution
+  'https://open.spotify.com/album/7us4WRnwvqCE3cppHVNjIu', // FTL
+  'https://open.spotify.com/album/6QtCv5E76lUVbXfUFZxLuH', // Fez
+  'https://open.spotify.com/album/5Q7I07l5PkrO5nDwepUdNE', // Chrono Trigger
+  'https://open.spotify.com/album/4ynYSjDmlRH6tc7hNWuv7J', // Hotline Miami
+  'https://open.spotify.com/album/7yRFTXb2wZoxI04MSK54o4', // Hotline Miami
+  'https://open.spotify.com/album/0Cqdcsm1RNSme7uDYfKssS', // Hotline Miami
+  // 'https://open.spotify.com/album/0KQyC28P9808r0oKKNgHvp', // Doom
+  // 'https://open.spotify.com/album/4aKKWSgxf7U7FN7njmi4MH', // God of War III
+  // 'https://open.spotify.com/album/7AeuCduXts5efoVLam7dUk', // Uncharted 2
+  // 'https://open.spotify.com/album/2Dc3L17J41tbywnJdJRPi7', // Uncharted 3
+  // 'https://open.spotify.com/album/1M07WV7nZNoqMAturqewUj', // Infamous
 ];
